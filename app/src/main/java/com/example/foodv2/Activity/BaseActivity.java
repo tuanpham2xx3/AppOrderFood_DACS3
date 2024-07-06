915b06e93df299b321cfcb5aba3878fa23ef069d
tunapahm2xx3 <niyanime282@gmail.com>
2024-07-06 23:24:32 +0700

    first commit

---
+++ app/src/main/java/com/example/foodv2/Activity/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.example.foodv2.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodv2.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class BaseActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    public String TAG="uilover";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        database = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+
+        getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+    }
+}
---
+++ app/src/main/java/com/example/foodv2/Helper/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package com.example.foodv2.Helper;
+
+public interface ChangeNumberItemsListener {
+    void change();
+}
